0fd01f665117cbb743bdb920968dc68eeec10c03
jplimam <limamarcelojoaopedro@gmail.com>
2025-01-31 09:58:14 -0300

    users crud with crypted password done

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import authRouter from "./auth/auth.js";
+import usersRouter from "./routes/users.js";
 
 const routes = new Router();
 
@@ -12,5 +13,6 @@ routes.get("/", (req, res) => {
 });
 
 routes.use("/auth", authRouter);
+routes.use("/users", usersRouter);
 
 export default routes;
